8b104dcb0e4a5b8c9c0446235555da6b9a2c815f
abardin <abardin@uci.edu>
2015-03-05 17:12:18 -0800

    greetings
    added 1 line

--- hello_world.m
+++ hello_world.m
@@ -2,3 +2,4 @@
 fprintf('hello world\n');
 % don't forget the \n  for the carriage return at the end of the line
 fprintf('I am still trying to figure out what it is \n')
+fprintf(‘Hello Earthling\n);
