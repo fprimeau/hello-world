c804a4df0dd38bde28a23b5e8a3b122ed6f45525
Francois Primeau <fprimeau@uci.edu>
2015-03-05 17:25:07 -0800

    welcome statement
    Added an extra fprintf statement

--- hello_world.m
+++ hello_world.m
@@ -2,4 +2,3 @@
 fprintf('hello world\n');
 % don't forget the \n  for the carriage return at the end of the line
 fprintf('I am still trying to figure out what it is \n')
-fprintf(‘Hello Earthling\n’);
