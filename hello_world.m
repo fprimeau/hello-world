b6b42f78cda1f80acc224dc52b056a8dbc125d33
Francois Primeau <fprimeau@uci.edu>
2015-03-05 17:39:46 -0800

    problem with quotes
    fixed the funny quote at the beginning of Hello Earthling

--- hello_world.m
+++ hello_world.m
@@ -2,5 +2,5 @@
 fprintf('hello world\n');
 % don't forget the \n  for the carriage return at the end of the line
 fprintf('I am still trying to figure out what it is \n')
-fprintf(‘Hello Earthling\n'); 
+fprintf('Hello Earthling\n'); 
 fprintf('Hello ET. Welcome to Earth.\n');
